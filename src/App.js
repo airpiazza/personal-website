0930a246d048b86b41ee069133deb299aa273579
airpiazza <nick@piazzaonline.com>
2022-08-31 17:15:02 -0700

    added background

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import sunsetBackground from './assets/sunset.jpg';
 
 function App() {
   return (
-    <div>
+    <div style={{
+      backgroundImage: `url(${sunsetBackground})`,
+      backgroundColor: 'black',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundAttachment: 'fixed',
+      minHeight: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
     </div>
   );
 }
