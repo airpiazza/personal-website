dab739c01b99f34c1ee47184e71dd0b6a2c6ba1b
airpiazza <nick@piazzaonline.com>
2022-09-08 12:12:24 -0700

    removed background image

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import './App.css';
-import sunsetBackground from './assets/sunset.jpg';
 
 function App() {
   return (
-    <div style={{
-      backgroundImage: `url(${sunsetBackground})`,
-      backgroundColor: 'black',
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center',
-      backgroundAttachment: 'fixed',
-      minHeight: '100vh',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center'
-    }}>
+    <div className="App">
     </div>
   );
 }
